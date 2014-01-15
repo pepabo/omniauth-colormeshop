d610d7e1ca735bac15091df750d893b4167b964b
Kentaro Kuribayashi <kentarok@gmail.com>
2014-01-15 09:49:07 +0900

    `bundle gem omniauth-cmsp`

---
+++ lib/omniauth/cmsp.rb
@@ -0,0 +1,7 @@
+require "omniauth/cmsp/version"
+
+module Omniauth
+  module Cmsp
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/cmsp/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Cmsp
+    VERSION = "0.0.1"
+  end
+end
